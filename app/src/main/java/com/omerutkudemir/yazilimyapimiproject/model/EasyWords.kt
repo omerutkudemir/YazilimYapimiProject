5bfbc58c99eca44c567fb098f8d47420f278a5c0
ömer utku demir <120313399+omerutkudemir@users.noreply.github.com>
2024-12-30 00:46:53 +0300

    Delete app/src/main/java/com/omerutkudemir/yazilimyapimiproject directory

--- app/src/main/java/com/omerutkudemir/yazilimyapimiproject/model/EasyWords.kt
+++
@@ -1,9 +0,0 @@
-package com.omerutkudemir.yazilimyapimiproject.model
-
-data class EasyWords(
-    val id:Int?,
-    val en:String?,
-    val tr:String?,
-    val sayi:Int?,
-    val imageUri:String?) {
-}
